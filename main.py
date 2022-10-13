2c50cb88a1fd47336846d3f2318982a87b0803e0
jhan daza <jhand@keoworld.com>
2022-10-13 10:23:41 -0500

    updates

--- main.py
+++ main.py
@@ -35,12 +35,11 @@ def hello_bot():
     
     # The following options are required to make headless Chrome
     # work in a Docker container
-    """ chrome_options = webdriver.ChromeOptions()
+    chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("window-size=1024,768")
-    chrome_options.add_argument("--no-sandbox") """
-
+    chrome_options.add_argument("--no-sandbox") 
 
     driver = webdriver.Chrome(chrome_options=opts)
 
