2ab41d4455f897b8c2c7fd23a8623901e2986b74
jhan daza <jhand@keoworld.com>
2022-10-25 14:34:59 -0500

    updates

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from selenium.webdriver.common.by import By
 from email.mime.multipart import MIMEMultipart
 from selenium.webdriver.chrome.options import Options
 import chromedriver_autoinstaller
-#from pyvirtualdisplay import Display  
+from pyvirtualdisplay import Display  
 
 
 
@@ -27,6 +27,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_bot():
+    disp = Display()
+    disp.start()
     chromedriver_autoinstaller.install()
     now = datetime.now() 
     year_month_day = now.strftime("%Y-%m-%d")
@@ -183,5 +185,6 @@ def hello_bot():
         pass
     time.sleep(1)
     driver.quit()
+    disp.stop()
     return df2
 
