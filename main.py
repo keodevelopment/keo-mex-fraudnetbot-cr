d0f3fe3cd59c44dba50a19679cd461c59d014db9
jhan daza <jhand@keoworld.com>
2022-12-23 20:31:09 -0500

    add flux

--- main.py
+++ main.py
@@ -98,16 +98,15 @@ def hello_bot():
         breakpoint
 
     try:
-        table = WebDriverWait(driver,180).until(
+        element = WebDriverWait(driver,180).until(
             EC.presence_of_element_located((By.XPATH, "//*[@id='responsiveWrapper_sub']/div[3]/div[2]/div/div/div[2]/div[2]/div/div[2]/table"))
-            print("tabla encontrada")
-            table = driver.find_element("xpath", '//*[@id="responsiveWrapper_sub"]/div[3]/div[2]/div/div/div[2]/div[2]/div/div[2]/table').get_attribute('outerHTML')
         )
+        print("tabla encontrada")
+        table = driver.find_element("xpath", '//*[@id="responsiveWrapper_sub"]/div[3]/div[2]/div/div/div[2]/div[2]/div/div[2]/table').get_attribute('outerHTML')
+        print("trying to find the reports")
     except:
-        print('No table!!')
+        print("No table")
         driver.quit()
-        breakpoint
-        
     
 
     df = pd.read_html(table)[0]  # Convert the table to a dataframe
